912c125d325594721f866e6c76c39e8dfd3c6340
Robin <liucangsong@foxmail.com>
2020-08-20 19:34:24 +0800

    New Project

--- src/main/java/Demo01.java
+++ src/main/java/Demo01.java
@@ -1,5 +1,8 @@
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("Hello!");
+        if (Math.random()>05){
+            System.out.println("OK");
+        }
     }
 }
