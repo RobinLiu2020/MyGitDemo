f5f28c00104244809e00093bead53d6ae1027e08
Robin <liucangsong@foxmail.com>
2020-08-20 19:40:00 +0800

    show date time

--- src/main/java/Demo01.java
+++
@@ -1,8 +0,0 @@
-public class Demo01 {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-        if (Math.random()>05){
-            System.out.println("OK");
-        }
-    }
-}
