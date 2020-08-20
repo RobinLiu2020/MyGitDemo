7d4f921db53b58a9bcd1f255cd1249f7ec8d2a13
Robin <liucangsong@foxmail.com>
2020-08-20 20:15:31 +0800

    new Version

--- src/main/java/Demo02.java
+++ src/main/java/Demo02.java
@@ -3,5 +3,6 @@ import java.time.LocalDateTime;
 public class Demo02 {
     public static void main(String[] args) {
         System.out.println(LocalDateTime.now());
+        System.out.println(LocalDateTime.now());
     }
 }
